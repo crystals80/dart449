1fdb92d31c03817aa01f878513a776c3cee769aa
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-14 21:52:32 -0500

    E3 Finished exercise 3
    by modifying the welcome message, adding the qualification matches section, simulating a gain/loss points and adding a disclaimer to the simulation

--- exercises/class5/js/script.js
+++ exercises/class5/js/script.js
@@ -7,12 +7,13 @@ Use arrays and loops to create a set of buttons that have the same class while g
 
 // (1) Display a reply when user answers using button (with a background)
 
-// Declare global variables
+// Declare global variables (and other global variables will be declared in groups with its functions)
 const yesBtn = document.querySelector('#yes');
 const noBtn = document.querySelector('#no');
 const alertMsg = document.getElementById('welcomeMsg');
 const waitBtn = document.querySelector('#wait');
-let reply = document.querySelector('#reply');
+const reply = document.querySelector('#reply');
+const team = document.querySelector('#team');
 
 // If "yes" button is clicked...
 yesBtn.addEventListener('click', function() {
@@ -22,12 +23,13 @@ yesBtn.addEventListener('click', function() {
   alertMsg.style.fontSize = "1.25em";
   alertMsg.style.color = "blue";
   alertMsg.style.marginBottom = "0";
-  alertMsg.innerHTML = `<h1>Welcome to the Bell Center!</h1>`;
+  alertMsg.innerHTML = `<h1>Welcome to the Skyscraper Bell Center!</h1>`;
   // Reply to user's answer
+  // document.getElementById('reply').style.visibility = 'visible';
   reply.style.color = "black";
-  reply.innerHTML = `<h2>Nice! Please choose your team!</h2>`;
+  reply.textContent = `Nice! Please choose your team!`;
   // Display next section of web page
-  nextContent();
+  document.getElementById('team').style.visibility = 'visible';
 });
 
 // If "no" button is clicked...
@@ -41,10 +43,13 @@ noBtn.addEventListener('click', function() {
   alertMsg.style.marginBottom = "0";
   alertMsg.innerHTML = `<h1>SECURITY BREACH</h1>`;
   // Reply to user's answer
+  // document.getElementById('reply').style.visibility = 'visible';
   reply.style.color = "red";
+  reply.style.textAlign = "center"
   reply.textContent = `Hello, James? Yes, please call the security team to escort this person out of here. You were right. This is the intruder`;
   // Hide new content
   document.getElementById('team').style.visibility = 'hidden';
+  // noSetTimeout();
 
   // Trigger a second change button
   waitBtn.style.display = "flex";
@@ -58,33 +63,69 @@ noBtn.addEventListener('click', function() {
     alertMsg.style.marginBottom = "0";
     alertMsg.innerHTML = `<h1>Welcome to the Bell Center!</h1>`;
     // Reply to user's answer
+    document.getElementById('reply').style.visibility = 'visible';
     reply.style.color = "black";
-    reply.style.textAlign = "center";
     // reply.style.margin = "0";
-    reply.innerHTML = `<p style="margin-top:1.5em; margin-bottom: 0.5em">Oh? Hold on, James. You would like to purchase a ticket?</p><p>Aw, you should have said so! All right James, false alert and remain in position!</p>`;
+    reply.innerHTML = `<p style="margin-top:1.5em; margin-bottom: 0.5em">Oh? Hold on, James.</p><p style="margin-bottom:0.5em">You would like to purchase a ticket? Aw, you should have said so!</p><p>All right James, false alert and remain in position!</p>`;
+    // Display next section of web page
+    document.getElementById('team').style.visibility = 'visible';
   });
-  // Display next section of web page
-  secondChance();
 });
 
-// (2) Display a set of buttons that can increase/decrease points and to disappear when clicked
+// (2) Display a set of buttons that has the same class
 
-// Initiate new content after 3 seconds
-function nextContent() {
-  setTimeout(() => {
-    chooseTeam()
-  }, 3000);
-}
-// Initiate new content after 7 seconds
-function nextContent() {
-  setTimeout(() => {
-    chooseTeam()
-  }, 7000);
-}
+// Display hockey teams in a list
+const hockeyTeams = [];
+const displayHockeyTeams = document.querySelector('#displayHockeyTeams');
+displayHockeyTeams.textContent = hockeyTeams.join(', ');
 
-// Show next section of webpage
-const team = document.querySelector('#team');
+const addTeam = document.querySelectorAll('.hockeyTeam');
 
-function chooseTeam() {
-  document.getElementById('team').style.visibility = 'visible';
+addTeam.forEach((btn) => {
+  btn.addEventListener('click', function() {
+    const vote = this.textContent;
+    console.log(vote);
+    hockeyTeams.push(vote);
+    displayHockeyTeams.textContent = hockeyTeams.join(', ');
+  });
+});
+
+// (3) Display a set of buttons that can increase/decrease points and to disappear when clicked
+let points = 0;
+const showPoints = document.querySelector('#pointsDisplay');
+const addPts = document.querySelector('.addPts');
+const substractPts = document.querySelector('.substractPts');
+
+const numAddPts = addPts.length;
+const numSubstractPts = substractPts.length;
+
+addPts.addEventListener('click', function() {
+  points += Math.floor(Math.random() * 10) + 1;
+  showPoints.textContent = `Total points: ${points}`;
+  if (points >= 100) {
+    win.style.color = 'green';
+    win.textContent = `${result} has been qualified for the quarter-final!`;
+    substractPts.style.display = 'none';
+  }
+});
+
+substractPts.addEventListener('click', function() {
+  points -= Math.floor(Math.random() * 10) + 1;
+  showPoints.textContent = `Total points: ${points}`;
+  if (points <= -100) {
+    win.style.color = 'red';
+    win.textContent = `${result} lost...Today...is bad luck!`;
+    addPts.style.display = 'none';
+  }
+});
+
+// Display a congratulating message when points reach +100 or -100
+const teamNames = ['Boston Bruins', 'Buffalo Sabres', 'Detroit Red Wings', 'Florida Panthers', 'Montreal Canadiens', 'Ottawa Senators', 'Tampa Bay Lightning', 'Toronto Maple Leaf'];
+const win = document.querySelector('#win');
+const result = winningMsg(teamNames);
+
+function winningMsg(arr) {
+  let randomNames = Math.floor(Math.random() * arr.length)
+  let chosen = arr[randomNames];
+  return chosen;
 }
