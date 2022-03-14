3c260754301bac2d78b0a7e98ef78d1e08bca4d2
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-13 21:47:14 -0400

    E4: Added progress bar

--- exercises/class6/js/script.js
+++ exercises/class6/js/script.js
@@ -5,6 +5,7 @@ Lam Ky Anh Do
 Use eventListeners, functions and conditional statements to create an example that asks for information and then changes text and an HTML element based on the information received.
 **********/
 
+// (1) Trigger transition effect of next section when horizontal scroll hits the middle of webpage
 gsap.registerPlugin(ScrollTrigger);
 
 const sections = gsap.utils.toArray("section");
@@ -48,9 +49,25 @@ ScrollTrigger.matchMedia({
         }
         // when the section intersects with the middle of the page
         // add the active class.
-        // animation is controlled in style.css line 69
+        // animation is controlled in style.css line 88
       });
     });
   }
-
 });
+
+
+// (2) Display a progress bar
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction();
+};
+
+function myFunction() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  console.log(`winScroll: ${winScroll}`);
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  console.log(`height: ${height}`);
+  const scrolled = (winScroll / height) * 100;
+  console.log(`scrolled: ${scrolled}%`);
+  document.getElementById('myBar').style.width = `${scrolled}%`;
+}
