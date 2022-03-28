7d4b0509e09be07f58e523ed17e8119b631634d4
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-28 17:07:30 -0400

    P2: Added an animated arrow

--- projects/project2/final/js/script.js
+++ projects/project2/final/js/script.js
@@ -1,73 +1,50 @@
-/**********
-Exercise 4: Using Functions, Conditional Statements and AddEventListeners
-Lam Ky Anh Do
+/****** CURSOR ******/
+const cursor = document.querySelector('.cursor');
+const cursorinner = document.querySelector('.cursor2');
+const a = document.querySelectorAll('a');
 
-Use eventListeners, functions and conditional statements to create an example that asks for information and then changes text and an HTML element based on the information received.
-**********/
-
-// (1) Trigger transition effect of next section when horizontal scroll hits the middle of webpage
-gsap.registerPlugin(ScrollTrigger);
-
-const sections = gsap.utils.toArray("section");
-
-// using matchMedia to activate scrollTrigger only when bigger than 700px
-// change this to a higher value than 700 if you want a tablet view size
-ScrollTrigger.matchMedia({
-  "(min-width:700px)": function() {
-
-    let maxWidth = 0;
-
-    const getMaxWidth = () => {
-      maxWidth = 0;
-      sections.forEach((section) => {
-        maxWidth += section.offsetWidth;
-      });
-    };
-    getMaxWidth();
-    ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
-
-    gsap.to(sections, {
-      x: () => `-${maxWidth - window.innerWidth}`,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".wrapper",
-        pin: true,
-        scrub: true,
-        end: () => `+=${maxWidth}`,
-        invalidateOnRefresh: true
-      }
-    });
-
-    sections.forEach((sct, i) => {
-      ScrollTrigger.create({
-        trigger: sct,
-        start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth / 2) * (maxWidth / (maxWidth - window.innerWidth)),
-        end: () => '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
-        toggleClass: {
-          targets: sct,
-          className: "active"
-        }
-        // when the section intersects with the middle of the page
-        // add the active class.
-        // animation is controlled in style.css line 88
-      });
-    });
-  }
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
 });
 
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  cursorinner.style.left = `${x}px`;
+  cursorinner.style.top = `${y}px`;
+});
 
-// (2) Display a progress bar
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-  myFunction();
-};
+document.addEventListener('mousedown', () => {
+  cursorinner.classList.add('cursorinnerhover');
+});
 
-function myFunction() {
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  console.log(`winScroll: ${winScroll}`);
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  console.log(`height: ${height}`);
-  const scrolled = (winScroll / height) * 100;
-  console.log(`scrolled: ${scrolled}%`);
-  document.getElementById('myBar').style.width = `${scrolled}%`;
-}
+document.addEventListener('mouseup', () => {
+  cursorinner.classList.remove('cursorinnerhover');
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+});
+
+/****** LANDING PAGE ARROW ******/
+
+// (1) Show arrow when user hover over turtle
+// $('#anim-turtle').hover(function() {
+//   $('#arrow').fadeIn(2000);
+// });
+
+const animTurtle = document.querySelector('#anim-turtle');
+const arrow = document.querySelector('#arrow');
+
+animTurtle.addEventListener('mouseover', function handleMouseOver() {
+  let timer = setTimeout(function() {
+    arrow.style.opacity = 1;
+  }, 1500);
+});
